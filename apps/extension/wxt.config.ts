0cd461759d1ebecb044ca74d0ae2d4935b88a07b
wessleyn <wessleynyakaz@gmail.com>
2025-05-17 10:38:48 +0200

    (chore) Rebased the current progress unto the dev branch

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -4,4 +4,16 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   srcDir: 'src',
+  manifest: {
+    name: 'Xianxu',
+    description: 'An extension to simplify and enhance your webnovel experience.',
+    action: {
+      default_title: 'Xianxu',
+    },
+  },
+  alias: {
+    '@': '/src',
+    '@assets': '/src/assets',
+    '@components': '/src/components',
+  }
 });
