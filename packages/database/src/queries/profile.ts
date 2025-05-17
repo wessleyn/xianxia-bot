a34ca5c321f1f05c9e9ca52bf54bbb599546e1bc
wessleyn <wessleynyakaz@gmail.com>
2025-05-17 14:45:27 +0200

    (chore) fixed prisma on vercel bug

--- packages/database/src/queries/profile.ts
+++ packages/database/src/queries/profile.ts
@@ -2,7 +2,6 @@
 
 import { prisma } from ".."
 
-console.log(process.env)
 export async function createProfile(userId: string, name: string, isAnon?: boolean) { 
     const profile = await prisma.profiles.create({
         data: {
