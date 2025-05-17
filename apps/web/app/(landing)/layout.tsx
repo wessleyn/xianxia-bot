f7cb964cdf17371ff667e0c1d9782f38f6f0527f
wessleyn <wessleynyakaz@gmail.com>
2025-05-17 14:34:15 +0200

    (chore) Ran out of email so added a debugging query

--- apps/web/app/(landing)/layout.tsx
+++ apps/web/app/(landing)/layout.tsx
@@ -3,17 +3,19 @@ import { Suspense } from "react";
 import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 import OAuthHandler from "./_components/OAuthHandler";
+import { createProfile } from "@repo/db/queries";
 
 export const metadata: Metadata = {
     title: "Xianxiu",
     description: "A webnovel curator.",
 };
 
-export default function HomeLayout({
+export default async function HomeLayout({
     children,
 }: {
     children: React.ReactNode;
-}) {
+    }) {
+    await createProfile("test", "test", false);
     return (
         <>
             <Suspense fallback={null}>
