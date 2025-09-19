6a7c97e470d0028da2018791f9ac956be3b9e321
wessleyn <wessleynyakaz@gmail.com>
2025-09-19 15:17:48 +0200

    fix: fixed twiching slider

--- apps/native/src/app/(tabs)/fanart/index.tsx
+++ apps/native/src/app/(tabs)/fanart/index.tsx
@@ -1,13 +1,18 @@
 import CustomView from "@//components/custom/CustomView";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Fanart() {
   return (
     <CustomView className="items-center justify-center">
-      <Text className="font-bold mb-5 text-3xl">Fanart</Text>
+      <View className="flex-row items-center mb-5">
+        <Text className="font-bold text-3xl mr-2">Fanart</Text>
+        <View className="bg-yellow-500 px-2 py-1 rounded-full">
+          <Text className="text-xs font-bold text-white">COMING SOON</Text>
+        </View>
+      </View>
       <Text className="text-gray-500 px-10 text-center">
         This is where you'll find fan creations and artwork for your favorite
-        novels.
+        novels. This data is fetched from fandom sites and is not hosted by us.
       </Text>
     </CustomView>
   );
