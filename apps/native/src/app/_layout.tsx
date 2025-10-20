f926b318d224f6a2e4eb81c7cead84b6ad6ac1ec
wessleyn <wessleynyakaz@gmail.com>
2025-10-20 07:08:45 +0200

    feat: update .gitignore and app.json for improved asset management and permissions

--- apps/native/src/app/_layout.tsx
+++ apps/native/src/app/_layout.tsx
@@ -4,6 +4,7 @@ import '@styles/global.css';
 import { supabase } from '@utils/supabase';
 import * as Font from 'expo-font';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
 import { SQLiteProvider } from 'expo-sqlite';
 import { useColorScheme } from 'nativewind';
 import { useEffect, useState } from 'react';
@@ -12,33 +13,32 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import 'react-native-url-polyfill/auto';
 
-const RootLayout = () => {
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
   const { setSession } = useAccountStore();
   const [appLoaded, setAppLoaded] = useState(false);
   const { setColorScheme } = useColorScheme();
 
   useEffect(() => {
-    async function loadFonts() {
+    async function loadApp() {
       await Font.loadAsync({
         'Verdana': require('./../../assets/fonts/Verdana/Verdana.ttf'),
         'Verdana-Bold': require('./../../assets/fonts/Verdana/Verdana-Bold.ttf'),
         'Verdana-Italic': require('./../../assets/fonts/Verdana/Verdana-Italic.ttf'),
         'Verdana-BoldItalic': require('./../../assets/fonts/Verdana/Verdana-BoldItalic.ttf'),
       });
-    }
 
-    async function checkColorTheme() {
       const theme = await AsyncStorage.getItem('theme')
       if (theme) {
         setColorScheme(theme as 'light' | 'dark' | 'system')
       }
     }
 
+    loadApp().then(() => setAppLoaded(true));
+
     const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
-      checkColorTheme();
-      loadFonts();
       setSession(session);
-      setAppLoaded(true);
     });
 
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -49,6 +49,12 @@ const RootLayout = () => {
       subscription.unsubscribe();
     };
   }, []);
+
+  useEffect(() => { 
+    if (appLoaded) {
+      SplashScreen.hideAsync();
+    }
+  },[appLoaded])
 
   if (!appLoaded) {
     return null;
@@ -73,5 +79,3 @@ const RootLayout = () => {
     </GestureHandlerRootView>
   )
 }
-
-export default RootLayout
